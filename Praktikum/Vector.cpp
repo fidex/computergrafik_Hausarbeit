142e0ead213e53ffa4a068da2de07afaea34f715
phil <phil@phil-PC>
2015-08-04 17:34:22 +0200

    A1 -1 Methode

--- Praktikum/Vector.cpp
+++ Praktikum/Vector.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Vector.h"
+#include <cmath>
 
 Vector::Vector() {
 }
@@ -27,7 +28,58 @@ float Vector::dot(const Vector& v) const {
 }
 
 Vector& Vector::normalize(){
-    
+    float length = this->length();
+    this->X = this->X/length;
+    this->Y = this->Y/length;
+    this->Z = this->Z/length;
+    return *this;
+}
+
+float Vector::length() const{
+    return sqrt(this->lengthSquared());
+}
+
+float Vector::lengthSquared() const{
+    return pow(this->X,2)+pow(this->Y,2)+pow(this->Z,2);
+}
+
+Vector Vector::operator+(const Vector& v) const {
+    Vector tmp(*this);
+    return tmp += v;
+}
+
+Vector Vector::operator-(const Vector& v) const {
+    Vector tmp(*this);
+    return tmp -= v;
+}
+
+Vector& Vector::operator+=(const Vector& v) {
+    this->X += v.X;
+    this->Y += v.Y;
+    this->Z += v.Z;
+    return *this;
+}
+
+Vector& Vector::operator-=(const Vector& v) {
+    this->X -= v.X;
+    this->Y -= v.Y;
+    this->Z -= v.Z;
+    return *this;
 }
 
 
+Vector Vector::operator*(float c) const {
+    return Vector(this->X*c, this->Y*c, this->Z*c);
+}
+
+
+Vector Vector::reflection(const Vector& normal) const {
+    Vector refl;
+    normal.normalize();
+    refl = *this - (normal*2.0*this->dot(normal));
+    return refl;
+}
+
+bool Vector::triangleIntersection(const Vector& d, const Vector& a, const Vector& b, const Vector& c, float& s) const {
+    
+}
