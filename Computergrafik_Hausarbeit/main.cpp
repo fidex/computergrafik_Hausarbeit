4f50c4d9af0f935a806d2ff9067105fa71dee956
fide <fide@fide-GE60-0NC-GE60-0ND>
2015-08-04 15:40:48 +0200

    ini

---
+++ Computergrafik_Hausarbeit/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fide
+ *
+ * Created on 4. August 2015, 15:40
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
